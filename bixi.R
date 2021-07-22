563165eb2091c6d85452378f30ae5632fa049463
KBMana <49214608+KBMana@users.noreply.github.com>
2021-07-22 06:16:50 -0400

    next step

--- bixi.R
+++ bixi.R
@@ -1,9 +1,22 @@
 library(sf)
 library(tidyverse)
+library(magrittr)
+library(stringr)
 
 bixi <- stations %>% 
-  st_as_sf(coords = c("longitude", "latitude") 
+  st_as_sf(coords = c("longitude", "latitude")) 
 
-           
+# import flows
+flow <- read.csv("OD_2020.csv") %>% 
+  separate(start_date, c("year", "month", "day"), "-") %>%
+  separate(day, c("day", "time"), " ")
+
+flow<- flow %>% 
+  separate(end_date, c("year_end", "month_end", "day_end"), "-") %>% 
+  separate(day_end, c("day_end", "time_end"), " ")
+
+flow %>% 
+  filter(month == month_end)
 
 
+glimpse(flow)
