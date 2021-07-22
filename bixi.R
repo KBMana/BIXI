65f38b11f2791092c3ca68f463a2cadc6e3e2127
KBMana <49214608+KBMana@users.noreply.github.com>
2021-07-22 11:10:56 -0400

    commit 2

--- bixi.R
+++ bixi.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(magrittr)
 library(stringr)
 
+# import stations
 bixi <- stations %>% 
   st_as_sf(coords = c("longitude", "latitude")) 
 
@@ -15,8 +16,6 @@ flow<- flow %>%
   separate(end_date, c("year_end", "month_end", "day_end"), "-") %>% 
   separate(day_end, c("day_end", "time_end"), " ")
 
-flow %>% 
-  filter(month == month_end)
 
 
 glimpse(flow)
